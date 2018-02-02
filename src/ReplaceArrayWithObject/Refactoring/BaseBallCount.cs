59895f27320cfed67e69f6676d054986af1e0b58
daniel.shih <daniel.shih@allpay.com.tw>
2018-02-02 09:28:08 +0800

    add src/ReplaceArrayWithObject

---
+++ src/ReplaceArrayWithObject/Refactoring/BaseBallCount.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplaceArrayWithObject.Refactoring
+{
+    /// <summary>
+    /// 將陣列封裝到一個類別中
+    /// </summary>
+    public class BaseBallCourt
+    {
+        string[] _baseBallCourt;
+
+        public BaseBallCourt(string[] baseballCourt)
+        {
+            this._baseBallCourt = baseballCourt;
+        }
+
+        public string Price {
+            get
+            {
+                return _baseBallCourt[0];
+            }
+            set
+            {
+                 _baseBallCourt[0] = value;
+            }
+        }
+
+        public string Boss
+        {
+            get
+            {
+                return _baseBallCourt[1];
+            }
+            set
+            {
+                _baseBallCourt[1] = value;
+            }
+        }
+
+        public string BuildOn
+        {
+            get
+            {
+                return _baseBallCourt[2];
+            }
+            set
+            {
+                _baseBallCourt[2] = value;
+            }
+        }
+    }
+}
---
+++ src/ReplaceArrayWithObject/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReplaceArrayWithObject.Refactoring;
+/*
+* 名稱：ReplaceArrayWithObject(使用物件替代陣列)
+* 
+* 說明：Array陣列(不只是Array存放集合都可以用此重構方法)
+* 使用連續記憶體存放同類型的數值，但使用陣列的Index並不知道他放物件的含意
+* 
+* 解決方式：使用一個物件的變數來賦予陣列欄位生命，而不是冷冰冰的記憶體空間(Index)..
+*
+* **/
+namespace ReplaceArrayWithObject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            #region Normal
+            string[] baseBallCourt = new string[100];
+            baseBallCourt[0] = "1000$";//Price
+            baseBallCourt[1] = "Tom";//Boss
+            baseBallCourt[2] = "1980";//Build on
+            //後續維護人員 必須依靠註解來了解 baseBall陣列的Index代表涵義。
+            //這個很不人性化，我們就可以把陣列封裝成一個物件 
+            #endregion
+
+            //將陣列封裝到物件中
+            BaseBallCourt court = new BaseBallCourt(baseBallCourt);
+
+            //測試通過...看起來美化很多了吧^^
+            Assert.AreEqual(court.Price, baseBallCourt[0]);
+            Assert.AreEqual(court.Boss, baseBallCourt[1]);
+            Assert.AreEqual(court.BuildOn, baseBallCourt[2]);
+        }
+    }
+}
